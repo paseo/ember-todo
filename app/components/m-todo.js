76fc868f3793ef007e1a749c49f3d9f1d117dabc
paseo <vularr@hotmail.com>
2016-02-12 19:32:25 +0800

    update

--- app/components/m-todo.js
+++ app/components/m-todo.js
@@ -4,11 +4,17 @@ export default Ember.Component.extend({
 	classNames:['list-group-item'],
 	isEditing: false,
 	store: Ember.inject.service(),
+	didRender() {
+		const isEditing = this.get('isEditing');
+		if(isEditing) {
+			this.$('.edit').focus();
+		}
+	},
 	actions:{
 		toggleComplete(e) {
-			const todo = this.get('record')
+			const todo = this.get('record');
 			const $checkbox = this.$('.toggle');
-			const checked = $checkbox.prop('checked')
+			const checked = $checkbox.prop('checked');
 
 			todo.set('isCompleted', checked);
 		},
